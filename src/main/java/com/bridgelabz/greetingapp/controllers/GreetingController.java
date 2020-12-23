823fbed73d6dd71ad9affac13595f953ebdda28e
madhuvayya <madhu.vayya@gmail.com>
2020-12-23 22:25:15 +0530

    Extended controller to use service layer to get greeting

--- src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controllers/GreetingController.java
@@ -2,20 +2,31 @@ package com.bridgelabz.greetingapp.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.greetingapp.dto.Greeting;
+import com.bridgelabz.greetingapp.service.GreetingService;
 
 @RestController
 public class GreetingController {
 
 	private static final String template="Hello, %s!";
-	private final AtomicLong counter=new AtomicLong();
+	private final AtomicLong counter= new AtomicLong();
+	
+	@Autowired
+	private GreetingService greetingService;
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name",defaultValue = "world") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/message")
+	public String greeting() {
+		return greetingService.greet(); 
 	}
 }
